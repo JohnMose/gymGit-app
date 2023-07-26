45f3750fc2bdb91f73abc24eb8428a7a63136ac4
MGisele <110932022+MGisele@users.noreply.github.com>
2023-07-26 16:54:05 +0300

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './HomePage.css';
+import { useNavigate } from 'react-router-dom';
 
-const HomePage = () => {
+const HomePage=()=>{
+  const navigate=useNavigate();
+
+  const handleSignClick=()=>{
+      navigate('/signup');
+  };
+  const handleLoginClick=()=>{
+    navigate('/login');
+  };
+     
+
+
   return (
     <div>
       <div className='containerHomePage'>
         <h1>GET FIT, HAVE FUN</h1>
-        <p>
-          Food delivers meals, drinks, and groceries from hundreds of restaurants and shops across countries directly to you.
-        </p>
-        <p1>Now you can get your favorite meal delivered right to your door! We've chosen the top delivery services in Kenya.</p1>
-
-        <p2>Everything you need,</p2>
-        <p3>delivered now</p3>
-
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
         
-        <Link to="/signup">
-          <button className="signup-button">Sign Up</button>
-        </Link>
-      </div>
+          
+          { <button onClick={handleLoginClick}>Login</button> }
+          <button onClick={handleSignClick}>Sign Up</button>
+          </div>
     </div>
   );
-}
+};
 
 export default HomePage;
